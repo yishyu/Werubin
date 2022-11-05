02b20caeb87de1ffffc55e5d668a02d7a439090f
Yishen Yu <yishen.yu@deliverect.com>
2022-11-05 10:59:22 +0100

    [Upd] log user at registration

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 from users.forms import RegistrationForm
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login as Login
 
 
 # Sign Up View
@@ -13,11 +14,16 @@ def registration(request):
             print("form is valid")
             user = form.save()
             user.set_password(form.cleaned_data['password1'])
-            user.email =
+            user.email = form.cleaned_data['email']
             user.save()
+            authuser = authenticate(username=user.username, password=form.cleaned_data['password1'])
+            if authuser:
+                Login(request, authuser)
             return HttpResponseRedirect(reverse("feeds:front_feed"))
         else:
-            print(form.errors.items())
+            return render(request, 'registration/registration.html', locals())
+
+    form = RegistrationForm()
     return render(request, 'registration/registration.html', locals())
 
 
