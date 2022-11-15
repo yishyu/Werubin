a1731dfffe2e536de79fcbde16d5547999bd701e
trcazier <tristan.gabriel.r.cazier@vub.be>
2022-11-15 18:32:47 +0100

    Refactored login & registration views + added single post page & post & comment template

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from feeds import views
+from feeds import views as views
 
 urlpatterns = [
     path("", views.front_feed, name='front_feed'),
+    path('post/<str:postID>/', views.singlePost, name='post')
 ]
--- feeds/views.py
+++ feeds/views.py
@@ -5,3 +5,28 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def front_feed(request):
     return render(request, 'base.html')
+
+@login_required
+def singlePost(request, postID):
+
+    # example variables
+    username = "Tristan Cazier"
+    postID=1000
+    location="Egypt"
+    profilepicture="https://cdn.discordapp.com/avatars/238355399634321408/a_0deed0f2ae15f2c36417675403066ddf.gif?size=1024"
+    picturelink="https://i.pinimg.com/736x/90/6f/99/906f99062b11c79e64f30c4a6be37ba3.jpg"
+    posttext="Looking for mummies"
+    numberofcomments=1
+    posttime="20 mins"
+
+    return render(request, 'posts/singlepostpage.html', 
+        { "username": username,
+          "postID": postID, 
+          "location": location,
+          "profilepicture": profilepicture,
+          "picturelink": picturelink,
+          "posttext": posttext,
+          "numberofcomments": numberofcomments,
+          "posttime": posttime
+        }
+    )
