e4500fce04a2f33406640ee0a9eda778f9ab713e
trcazier <tristan.gabriel.r.cazier@vub.be>
2022-11-22 21:31:33 +0100

    [Upd] Refactored CSS + Updated post layout

--- feeds/views.py
+++ feeds/views.py
@@ -13,10 +13,13 @@ def singlePost(request, postID):
     username = "Tristan Cazier"
     postID=1000
     location="Egypt"
+    hashtags="#Egypt, #Mummy, #Pyramids"
     profilepicture="https://cdn.discordapp.com/avatars/238355399634321408/a_0deed0f2ae15f2c36417675403066ddf.gif?size=1024"
     picturelink="https://i.pinimg.com/736x/90/6f/99/906f99062b11c79e64f30c4a6be37ba3.jpg"
+    map=""
     posttext="Looking for mummies"
     numberofcomments=1
-    posttime="20 mins"
+    posttime="2 November 2022, 22:12"
+    
 
     return render(request, 'posts/singlepostpage.html', locals())
