ccbb361de48a58c2cc48042b77165174bb66e79e
Yishen Yu <yishen.yu@deliverect.com>
2022-12-04 13:14:48 +0100

    [Add] location and tag on post

--- travels/models.py
+++ travels/models.py
@@ -13,6 +13,8 @@ class Post(models.Model):
             - can be shared
             - can be commented
             - can be liked
+            - has a location
+            - has tags
     """
     content = models.TextField("Text content")
     creation_date = models.DateTimeField("creation date", auto_now=False, auto_now_add=True)
@@ -20,10 +22,28 @@ class Post(models.Model):
     author = models.ForeignKey("users.User", verbose_name="User", on_delete=models.CASCADE, related_name="author")
     shares = models.ForeignKey("self", verbose_name="Shared post", on_delete=models.CASCADE, null=True, blank=True)  # for me sharig a post means, creating a new post containing the post
     likes = models.ManyToManyField("users.User", verbose_name="Likes", related_name="likes", blank=True)
+    location = models.ForeignKey("Location", verbose_name="Location", on_delete=models.CASCADE, null=True, blank=True)
+    tags = models.ManyToManyField("Tag", verbose_name="Tags", blank=True)
 
     @property
     def short_description(self):
         return truncatechars(self.content, 100)
+
+
+class Tag(models.Model):
+    name = models.CharField("name", max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField("Location Place Name", max_length=50)
+    lat = models.CharField("latitude", max_length=50, null=True, blank=True)
+    lng = models.CharField("longitude", max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Comment(models.Model):
@@ -34,7 +54,14 @@ class Comment(models.Model):
     content = models.TextField("Text content")
     post = models.ForeignKey("travels.Post", verbose_name="Post", on_delete=models.CASCADE)
     creation_date = models.DateTimeField("creation date", auto_now=False, auto_now_add=True)
-    likes = models.ManyToManyField("users.User", verbose_name="Likes", related_name="Commentlikes")
+    likes = models.ManyToManyField("users.User", verbose_name="Likes", related_name="Commentlikes", blank=True)
+
+    @property
+    def time_ago(self):
+        '''
+        TODO method return now - creation_date => min if < 1h, hour if < 1 day, day
+        '''
+        return "8 min"
 
 
 class PostImage(models.Model):
