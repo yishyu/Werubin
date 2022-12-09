df2645542750347632d47fa86d514e7d3bb2a638
Yishen Yu <yishen.yu@deliverect.com>
2022-12-09 19:47:54 +0100

    [Upd] dynamic feed

--- feeds/api_urls.py
+++ feeds/api_urls.py
@@ -3,6 +3,5 @@ import feeds.api_views as views
 
 app_name = "feeds_api"
 urlpatterns = [
-    path("all_posts/", views.all_posts, name="all_posts"),
-
+    path("feed/", views.feed, name="feed"),
 ]
