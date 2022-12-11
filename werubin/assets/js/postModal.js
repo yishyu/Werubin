8a759b4144d26cd273d4a9b63de9dbb98a8051f2
trcazier <tristan.gabriel.r.cazier@vub.be>
2022-12-11 16:06:08 +0100

    [Upd] refactored cdn/js

---
+++ werubin/assets/js/postModal.js
@@ -0,0 +1,29 @@
+function closePostModal(){
+    $("#post-modal").modal('hide');
+}
+
+function openPostModal() {
+    $("#post-modal").modal('show');
+}    
+
+var currentTagFieldId = 1
+function addNewTagField() {
+    let tagFieldId = "postTag" + currentTagFieldId
+    currentTagFieldId += 1
+
+    $("#addTag").remove()
+
+    let postTagHTML = `
+        <div class="row">
+            <div class="form-group col-10">
+                <input class="tagInput center form-control" id="${tagFieldId}" type="text" name="${tagFieldId}" placeholder="Enter a tag...">
+            </div>
+            <div id="addTag" class="center col-1 btn btn-outline-warning" onclick="addNewTagField()">
+                +
+            </div>
+        </div>
+    `
+
+    $("#tagInputs").append(postTagHTML)
+
+}
