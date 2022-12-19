051963e8926bf4aa03aa122ae50575f2a36acb94
yishyu <yishen.yu@chifuri.be>
2022-12-19 23:36:53 +0100

    [Fix] Yousri

--- werubin/settings.py
+++ werubin/settings.py
@@ -54,8 +54,8 @@ AUTH_USER_MODEL = 'users.User'
 
 EMAIL_HOST = 'ssl0.ovh.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'werubin.yueat'
-EMAIL_HOST_PASSWORD = 'ab'
+EMAIL_HOST_USER = env("EMAIL_HOST")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
 
 # Application definition
