33d820d02bbb22805139636bdcb942d70f28bec4
yishyu <yishen.yu@chifuri.be>
2022-12-20 00:07:28 +0100

    [Upd] comment author href

--- werubin/assets/js/post.js
+++ werubin/assets/js/post.js
@@ -304,7 +304,7 @@ function add_comment(postId, comment){
     comment = `
     <div class="d-flex flex-row mb-2"> <img src="${picture}" width=50 height=50 class="rounded-image">
         <div class="d-flex flex-column ml-2">
-            <span class="name"><a href="#" class="yellow-link"><u>${comment.author.username}</u></a></span>
+            <span class="name"><a href="/users/profile/${comment.author.username}" class="yellow-link"><u>${comment.author.username}</u></a></span>
             <span class="comment-text yellow-darker-text">${comment.content}</span>
             <div class="d-flex flex-row align-items-center status yellow-text">
                 <small><a id="comment-like-button${comment.id}" href="javascript:void(0);" onclick="like_comment(${comment.id})" class="${like_color}"><i class="bi bi-hand-thumbs-up-fill"></i> Like</a></small>
